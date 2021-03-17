f6dfa0568e3a460ac456495216d51eac68cddcac
Alperen <alperenoz1464@gmail.com>
2021-03-17 22:03:55 +0300

    entities created

---
+++ NLayerArchitecture.Core/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerArchitecture.Core.Entities
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ NLayerArchitecture.Core/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NLayerArchitecture.Core.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Stock { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool IsDeleted { get; set; }
+        public string InnerBarcode { get; set; }
+        public virtual Category Category { get; set; } //virtual??
+
+    }
+}
