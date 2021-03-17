4e020ce3ad56265cc058bca4c73b5299733ebe28
Alperen <alperenoz1464@gmail.com>
2021-03-17 22:34:04 +0300

    repositories in Core Layer are done.

---
+++ NLayerArchitecture.Core/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using NLayerArchitecture.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerArchitecture.Core.Repositories
+{
+    interface ICategoryRepository: IRepository<Category>
+    {
+        Task<Category> GetWithProductsById(int categoryId);
+    }
+}
---
+++ NLayerArchitecture.Core/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using NLayerArchitecture.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerArchitecture.Core.Repositories
+{
+    interface IProductRepository: IRepository<Product>
+    {
+        Task<Product> GetWithCategoryByIdAsync(int productId);
+    }
+}
