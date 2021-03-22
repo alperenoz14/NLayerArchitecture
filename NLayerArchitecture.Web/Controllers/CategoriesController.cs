ae10c9a23861d13e76cabab15d7eed020718d2a3
Alperen <alperenoz1464@gmail.com>
2021-03-22 15:04:19 +0300

    update and remove part has problem?

---
+++ NLayerArchitecture.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayerArchitecture.API.DTO_s;
+using NLayerArchitecture.Core.Entities;
+using NLayerArchitecture.Web.APIService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayerArchitecture.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly CategoryAPIService _categoryAPIService;
+        private readonly IMapper _mapper;
+        public CategoriesController(CategoryAPIService categoryAPIService, IMapper mapper)
+        {
+            _categoryAPIService = categoryAPIService;
+            _mapper = mapper;
+
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var categories = await _categoryAPIService.GetAllAsync();
+
+            return View(_mapper.Map<IEnumerable<CategoryDto>>(categories));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            await _categoryAPIService.AddAsync(categoryDto);
+
+            return RedirectToAction("List");
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var category = await _categoryAPIService.GetByIdAsync(id);
+
+            return View(_mapper.Map<CategoryDto>(category));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(CategoryDto categoryDto)
+        {
+            await _categoryAPIService.Update(categoryDto);
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _categoryAPIService.Remove(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- NLayerArchitecture.Web/Startup.cs
+++ NLayerArchitecture.Web/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NLayerArchitecture.Web.APIService;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,11 @@ namespace NLayerArchitecture.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<CategoryAPIService>(opt =>
+            {
+                opt.BaseAddress = new Uri(Configuration["baseUrl"]);
+            });
+            services.AddAutoMapper(typeof(Startup));
             services.AddMvc();
             services.AddControllersWithViews();
         }
